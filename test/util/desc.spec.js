dbcdd09c1098ddcc4dad90277544abe00734cf1b
Lukas Siemon <lukas.siemon@gmail.com>
2019-08-22 20:57:41 -0700

    fix: updated deps

--- test/util/desc.spec.js
+++ test/util/desc.spec.js
@@ -8,52 +8,7 @@ const describe = require('../../src/util/desc');
 
 const dirPrefix = path.join(os.tmpdir(), 'tmp-');
 
-describe('Testing useTmpDir', () => {
-  describe('Testing Custom Before/After', { useTmpDir: true }, () => {
-    let beforeDir;
-
-    beforeEach(({ dir }) => {
-      assert(dir.startsWith(dirPrefix));
-      beforeDir = dir;
-    });
-
-    afterEach(({ dir }) => {
-      assert(dir === undefined);
-    });
-
-    it('Testing dir matched beforeEach dir', ({ dir }) => {
-      expect(dir).to.equal(beforeDir);
-    });
-  });
-
-  describe('Testing Defaults', { useTmpDir: true }, () => {
-    it('Testing dir stars with prefix', ({ dir }) => {
-      expect(dir.startsWith(dirPrefix)).to.equal(true);
-    });
-  });
-
-  describe('Testing useTmpDir not set', () => {
-    it('Testing dir is null', ({ dir }) => {
-      expect(dir).to.equal(undefined);
-    });
-  });
-});
-
-describe('Testing useNock', { useNock: true }, () => {
-  it('Testing useNock empty recording', () => {});
-
-  it('Testing useNock record request', async () => {
-    const result = await request({
-      uri: 'http://ip-api.com/json',
-      method: 'GET',
-      json: true,
-      resolveWithFullResponse: true
-    });
-    expect(result.headers.date).to.equal('Sun, 19 Nov 2017 02:02:30 GMT');
-  });
-});
-
-describe('Testing environment variables', () => {
+describe('Testing { describe }', () => {
   before(() => {
     assert(process.env.VAR === 'VALUE');
   });
@@ -97,105 +52,153 @@ describe('Testing environment variables', () => {
       expect(process.env.VAR).to.equal('OTHER');
     });
   });
-});
 
-describe('Testing freezing time', { timestamp: 123456789 }, () => {
-  before(() => {
-    assert(Math.floor(new Date() / 1000) === 123456789);
-  });
+  describe('Testing useTmpDir', () => {
+    describe('Testing Custom Before/After', { useTmpDir: true }, () => {
+      let beforeDir;
 
-  after(() => {
-    assert(Math.floor(new Date() / 1000) > 123456789);
-  });
+      beforeEach(({ dir }) => {
+        assert(dir.startsWith(dirPrefix));
+        beforeDir = dir;
+      });
 
-  it('Testing time is frozen', () => {
-    expect(Math.floor(new Date() / 1000)).to.equal(123456789);
-  });
-});
+      afterEach(({ dir }) => {
+        assert(dir === undefined);
+      });
 
-describe('Testing console recording', { recordConsole: true }, () => {
-  const logger = ['log', 'info', 'error', 'warn'].reduce((p, c) => Object.assign(p, {
-    // eslint-disable-next-line no-console
-    [c]: (...args) => console[c](...args)
-  }), {});
-
-  it('Testing recorded logs', ({ getConsoleOutput }) => {
-    expect(getConsoleOutput()).to.deep.equal([]);
-    ['log', 'warn', 'error', 'info'].forEach((level) => {
-      logger[level](level);
+      it('Testing dir matched beforeEach dir', ({ dir }) => {
+        expect(dir).to.equal(beforeDir);
+      });
     });
-    const result = getConsoleOutput();
-    expect(result).to.deep.equal(['log', 'warn', 'error', 'info']);
-    ['log', 'warn', 'error', 'info'].forEach((level) => {
-      expect(result[level]).to.deep.equal([level]);
+
+    describe('Testing Defaults', { useTmpDir: true }, () => {
+      it('Testing dir stars with prefix', ({ dir }) => {
+        expect(dir.startsWith(dirPrefix)).to.equal(true);
+      });
+    });
+
+    describe('Testing useTmpDir not set', () => {
+      it('Testing dir is null', ({ dir }) => {
+        expect(dir).to.equal(undefined);
+      });
     });
   });
 
-  it('Testing recording resets', ({ getConsoleOutput }) => {
-    expect(getConsoleOutput()).to.deep.equal([]);
-    logger.log('log');
-    expect(getConsoleOutput()).to.deep.equal(['log']);
-    expect(getConsoleOutput().error).to.deep.equal([]);
-  });
-});
+  describe('Testing useNock', { useNock: true }, () => {
+    it('Testing useNock empty recording', () => {});
 
-describe('Testing random mocking', { cryptoSeed: 'ca8e7655-cd4f-47bf-a817-3b44f0f5b74e' }, () => {
-  it('Testing random is mocked', () => {
-    expect(uuid4()).to.deep.equal('f052644d-e485-4693-aef0-76267f1499ea');
-    expect(uuid4()).to.deep.equal('ba8e46ec-d63d-4fb8-9189-23e2454f7172');
-  });
-});
-
-describe('Testing async done callback', () => {
-  it('Test done is a function', (done) => {
-    expect(typeof done).to.equal('function');
-    done();
-  });
-});
-
-describe('Testing capture', () => {
-  it('Test capture captures error', async ({ capture }) => {
-    const error = new Error();
-    const e = await capture(() => {
-      throw error;
+    it('Testing useNock record request', async () => {
+      const result = await request({
+        uri: 'http://ip-api.com/json',
+        method: 'GET',
+        json: true,
+        resolveWithFullResponse: true
+      });
+      expect(result.headers.date).to.equal('Sun, 19 Nov 2017 02:02:30 GMT');
     });
-    expect(e).to.equal(error);
   });
 
-  it('Test capture throws when no error captured', async ({ capture }) => {
-    try {
-      await capture(() => {});
-    } catch (e) {
-      expect(String(e)).to.equal('AssertionError [ERR_ASSERTION]: expected [Function] to throw an error');
-    }
-  });
-});
+  describe('Testing freezing time', { timestamp: 123456789 }, () => {
+    before(() => {
+      assert(Math.floor(new Date() / 1000) === 123456789);
+    });
 
-describe('Testing Before/After', () => {
-  const state = [];
+    after(() => {
+      assert(Math.floor(new Date() / 1000) > 123456789);
+    });
 
-  before(() => {
-    state.push('before');
+    it('Testing time is frozen', () => {
+      expect(Math.floor(new Date() / 1000)).to.equal(123456789);
+    });
   });
 
-  after(() => {
-    state.push('after');
-    assert(state.join('-') === 'before-beforeEach-testOne-afterEach-beforeEach-testTwo-afterEach-after');
+  describe('Testing console recording', { recordConsole: true }, () => {
+    let logger;
+    before(() => {
+      logger = ['log', 'info', 'error', 'warn'].reduce((p, c) => Object.assign(p, {
+        // eslint-disable-next-line no-console
+        [c]: (...args) => console[c](...args)
+      }), {});
+    });
+
+    it('Testing recorded logs', ({ getConsoleOutput }) => {
+      expect(getConsoleOutput()).to.deep.equal([]);
+      ['log', 'warn', 'error', 'info'].forEach((level) => {
+        logger[level](level);
+      });
+      const result = getConsoleOutput();
+      expect(result).to.deep.equal(['log', 'warn', 'error', 'info']);
+      ['log', 'warn', 'error', 'info'].forEach((level) => {
+        expect(result[level]).to.deep.equal([level]);
+      });
+    });
+
+    it('Testing recording resets', ({ getConsoleOutput }) => {
+      expect(getConsoleOutput()).to.deep.equal([]);
+      logger.log('log');
+      expect(getConsoleOutput()).to.deep.equal(['log']);
+      expect(getConsoleOutput().error).to.deep.equal([]);
+    });
   });
 
-  beforeEach(() => {
-    state.push('beforeEach');
+  describe('Testing random mocking', { cryptoSeed: 'ca8e7655-cd4f-47bf-a817-3b44f0f5b74e' }, () => {
+    it('Testing random is mocked', () => {
+      expect(uuid4()).to.deep.equal('f052644d-e485-4693-aef0-76267f1499ea');
+      expect(uuid4()).to.deep.equal('ba8e46ec-d63d-4fb8-9189-23e2454f7172');
+    });
   });
 
-  afterEach(() => {
-    state.push('afterEach');
+  describe('Testing async done callback', () => {
+    it('Test done is a function', (done) => {
+      expect(typeof done).to.equal('function');
+      done();
+    });
   });
 
-  it('Test one', () => {
-    state.push('testOne');
+  describe('Testing capture', () => {
+    it('Test capture captures error', async ({ capture }) => {
+      const error = new Error();
+      const e = await capture(() => {
+        throw error;
+      });
+      expect(e).to.equal(error);
+    });
+
+    it('Test capture throws when no error captured', async ({ capture }) => {
+      try {
+        await capture(() => {});
+      } catch (e) {
+        expect(String(e)).to.equal('AssertionError [ERR_ASSERTION]: expected [Function] to throw an error');
+      }
+    });
   });
 
-  it('Test two', () => {
-    state.push('testTwo');
+  describe('Testing Before/After', () => {
+    const state = [];
+
+    before(() => {
+      state.push('before');
+    });
+
+    after(() => {
+      state.push('after');
+      assert(state.join('-') === 'before-beforeEach-testOne-afterEach-beforeEach-testTwo-afterEach-after');
+    });
+
+    beforeEach(() => {
+      state.push('beforeEach');
+    });
+
+    afterEach(() => {
+      state.push('afterEach');
+    });
+
+    it('Test one', () => {
+      state.push('testOne');
+    });
+
+    it('Test two', () => {
+      state.push('testTwo');
+    });
   });
 });
