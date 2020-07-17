d40abd8706aebfbdd0eea5e5298bc9716dc0f0ce
Lukas Siemon <github@blackflux.com>
2020-07-16 21:12:13 -0700

    amend: minor

--- src/modules/request-recorder/apply-modifiers.js
+++ src/modules/request-recorder/apply-modifiers.js
@@ -5,18 +5,17 @@ module.exports = (input, modifiers) => {
     filterFn: ({ key, value, parent }) => {
       const k = key[key.length - 1];
       if (k.includes('|')) {
-        const [prefix, ...toApply] = k.split('|');
+        const [prefix, ...modifierNames] = k.split('|');
         let newKey = prefix;
         let newValue = value;
-        for (let idx = 0; idx < toApply.length; idx += 1) {
-          const modifierName = toApply[idx];
+        modifierNames.forEach((modifierName) => {
           const modifier = modifiers[modifierName];
           if (typeof modifier === 'function') {
             newValue = modifier(newValue);
           } else {
             newKey += `|${modifierName}`;
           }
-        }
+        });
         if (k !== newKey) {
           // eslint-disable-next-line no-param-reassign
           delete parent[k];
