15eaaf55c121e153bf334d88fea407dc5eaeac92
Lukas Siemon <lukas.siemon@blackflux.com>
2023-08-04 14:45:55 -0700

    amend: test fixes

--- src/modules/cache-clearer.js
+++ src/modules/cache-clearer.js
@@ -11,12 +11,11 @@ const getFns = (obj) => {
   }
   for (let i = 0; i < properties.length; i += 1) {
     const key = properties[i];
-    const value = LRU.prototype[key];
-    if (typeof value !== 'function') {
-      // eslint-disable-next-line no-continue
-      continue;
-    }
-    result.push({ obj, key, value });
+    try {
+      const value = LRU.prototype[key];
+      assert(typeof value === 'function');
+      result.push({ obj, key, value });
+    } catch (e) { /* ignored */ }
   }
   return result;
 };
@@ -32,23 +31,19 @@ export default () => {
     inject: () => {
       assert(injected === false);
       fns.forEach(({ obj, key, value }) => {
-        try {
-          // eslint-disable-next-line no-param-reassign,func-names
-          obj[key] = function (...args) {
-            caches.push(this);
-            return value.call(this, ...args);
-          };
-        } catch (e) { /* ignored */ }
+        // eslint-disable-next-line no-param-reassign,func-names
+        obj[key] = function (...args) {
+          caches.push(this);
+          return value.call(this, ...args);
+        };
       });
       injected = true;
     },
     release: () => {
       assert(injected === true);
       fns.forEach(({ obj, key, value }) => {
-        try {
-          // eslint-disable-next-line no-param-reassign
-          obj[key] = value;
-        } catch (e) { /* ignored */ }
+        // eslint-disable-next-line no-param-reassign
+        obj[key] = value;
       });
       caches.splice(0).forEach((c) => c.clear());
       injected = false;
