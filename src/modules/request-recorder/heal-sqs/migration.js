906b11703de5410bbbe74a90d3aaf9f27ebce6c6
Lukas Siemon <lukas.siemon@blackflux.com>
2023-12-01 15:33:07 -0800

    amend: coverage work

--- src/modules/request-recorder/heal-sqs/migration.js
+++ src/modules/request-recorder/heal-sqs/migration.js
@@ -16,92 +16,91 @@ const tryParseXML = (body) => {
 
 export default ({ responseBody, header }) => {
   const responseXml = tryParseXML(responseBody);
-  if (responseXml === null) {
-    return {};
-  }
-  if (header === 'AmazonSQS.ListQueueTags') {
-    const scanner = objectScan(['ListQueueTagsResponse.ListQueueTagsResult[0].Tag[*]'], {
-      rtn: ({ value }) => [value.Key, value.Value[0]],
-      afterFn: ({ result }) => Object.fromEntries(result)
-    });
-    const Tags = scanner(responseXml);
-    return { Tags };
-  }
-  if (header === 'AmazonSQS.GetQueueAttributes') {
-    const scanner = objectScan(['GetQueueAttributesResponse.GetQueueAttributesResult[0].Attribute[*]'], {
-      rtn: ({ value }) => [value.Name[0], value.Value[0]],
-      afterFn: ({ result }) => Object.fromEntries(result)
-    });
-    const Attributes = scanner(responseXml);
-    return { Attributes };
-  }
-  if (header === 'AmazonSQS.GetQueueUrl') {
-    if (responseXml?.ErrorResponse?.Error?.[0]?.Code?.[0] === 'AWS.SimpleQueueService.NonExistentQueue') {
+  if (responseXml !== null) {
+    if (header === 'AmazonSQS.ListQueueTags') {
+      const scanner = objectScan(['ListQueueTagsResponse.ListQueueTagsResult[0].Tag[*]'], {
+        rtn: ({ value }) => [value.Key, value.Value[0]],
+        afterFn: ({ result }) => Object.fromEntries(result)
+      });
+      const Tags = scanner(responseXml);
+      return { Tags };
+    }
+    if (header === 'AmazonSQS.GetQueueAttributes') {
+      const scanner = objectScan(['GetQueueAttributesResponse.GetQueueAttributesResult[0].Attribute[*]'], {
+        rtn: ({ value }) => [value.Name[0], value.Value[0]],
+        afterFn: ({ result }) => Object.fromEntries(result)
+      });
+      const Attributes = scanner(responseXml);
+      return { Attributes };
+    }
+    if (header === 'AmazonSQS.GetQueueUrl') {
+      if (responseXml?.ErrorResponse?.Error?.[0]?.Code?.[0] === 'AWS.SimpleQueueService.NonExistentQueue') {
+        return {
+          __type: 'com.amazonaws.sqs#QueueDoesNotExist',
+          message: 'The specified queue does not exist.'
+        };
+      }
+      const QueueUrl = responseXml?.GetQueueUrlResponse?.GetQueueUrlResult?.[0]?.QueueUrl?.[0];
+      return { QueueUrl };
+    }
+    if (header === 'AmazonSQS.CreateQueue') {
+      if (responseXml?.ErrorResponse?.Error?.[0]?.Code?.[0] === 'QueueAlreadyExists') {
+        return {
+          __type: 'com.amazonaws.sqs#QueueNameExists',
+          message: 'The specified queue name does exist.'
+        };
+      }
+      const QueueUrl = responseXml?.CreateQueueResponse?.CreateQueueResult?.[0]?.QueueUrl?.[0];
+      return { QueueUrl };
+    }
+    if (header === 'AmazonSQS.ListQueues') {
+      const scannerQueueUrls = objectScan(
+        ['ListQueuesResponse.ListQueuesResult[0].QueueUrl[*]'],
+        { rtn: 'value', reverse: false }
+      );
+      const scannerNextToken = objectScan(
+        ['ListQueuesResponse.ListQueuesResult[0].NextToken[0]'],
+        { rtn: 'value', reverse: false, abort: true }
+      );
       return {
-        __type: 'com.amazonaws.sqs#QueueDoesNotExist',
-        message: 'The specified queue does not exist.'
+        QueueUrls: scannerQueueUrls(responseXml),
+        NextToken: scannerNextToken(responseXml)
       };
     }
-    const QueueUrl = responseXml?.GetQueueUrlResponse?.GetQueueUrlResult?.[0]?.QueueUrl?.[0];
-    return { QueueUrl };
-  }
-  if (header === 'AmazonSQS.CreateQueue') {
-    if (responseXml?.ErrorResponse?.Error?.[0]?.Code?.[0] === 'QueueAlreadyExists') {
+    if (header === 'AmazonSQS.TagQueue') {
+      return {};
+    }
+    if (header === 'AmazonSQS.SetQueueAttributes') {
+      return {};
+    }
+    if (header === 'AmazonSQS.SendMessageBatch') {
+      const scannerSuccessful = objectScan(
+        ['SendMessageBatchResponse.SendMessageBatchResult[0].SendMessageBatchResultEntry[*]'],
+        {
+          rtn: ({ value }) => ({
+            Id: value.Id[0],
+            MessageId: value.MessageId[0],
+            MD5OfMessageBody: value.MD5OfMessageBody[0]
+          }),
+          reverse: false
+        }
+      );
+      const scannerFailed = objectScan(
+        ['SendMessageBatchResponse.SendMessageBatchResult[0].BatchResultErrorEntry[*]'],
+        {
+          rtn: ({ value }) => ({
+            Id: value.Id[0],
+            SenderFault: value.SenderFault[0],
+            Code: value.Code[0]
+          }),
+          reverse: false
+        }
+      );
       return {
-        __type: 'com.amazonaws.sqs#QueueNameExists',
-        message: 'The specified queue name does exist.'
+        Successful: scannerSuccessful(responseXml),
+        Failed: scannerFailed(responseXml)
       };
     }
-    const QueueUrl = responseXml?.CreateQueueResponse?.CreateQueueResult?.[0]?.QueueUrl?.[0];
-    return { QueueUrl };
-  }
-  if (header === 'AmazonSQS.ListQueues') {
-    const scannerQueueUrls = objectScan(
-      ['ListQueuesResponse.ListQueuesResult[0].QueueUrl[*]'],
-      { rtn: 'value', reverse: false }
-    );
-    const scannerNextToken = objectScan(
-      ['ListQueuesResponse.ListQueuesResult[0].NextToken[0]'],
-      { rtn: 'value', reverse: false, abort: true }
-    );
-    return {
-      QueueUrls: scannerQueueUrls(responseXml),
-      NextToken: scannerNextToken(responseXml)
-    };
-  }
-  if (header === 'AmazonSQS.TagQueue') {
-    return {};
-  }
-  if (header === 'AmazonSQS.SetQueueAttributes') {
-    return {};
-  }
-  if (header === 'AmazonSQS.SendMessageBatch') {
-    const scannerSuccessful = objectScan(
-      ['SendMessageBatchResponse.SendMessageBatchResult[0].SendMessageBatchResultEntry[*]'],
-      {
-        rtn: ({ value }) => ({
-          Id: value.Id[0],
-          MessageId: value.MessageId[0],
-          MD5OfMessageBody: value.MD5OfMessageBody[0]
-        }),
-        reverse: false
-      }
-    );
-    const scannerFailed = objectScan(
-      ['SendMessageBatchResponse.SendMessageBatchResult[0].BatchResultErrorEntry[*]'],
-      {
-        rtn: ({ value }) => ({
-          Id: value.Id[0],
-          SenderFault: value.SenderFault[0],
-          Code: value.Code[0]
-        }),
-        reverse: false
-      }
-    );
-    return {
-      Successful: scannerSuccessful(responseXml),
-      Failed: scannerFailed(responseXml)
-    };
   }
   return responseBody;
 };
